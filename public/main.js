3821589f0b1ffdd361d116367063880370154c1d
sudhakar3697 <rsudhakar3697@gmail.com>
2020-07-06 17:22:01 +0530

    Added main.js

---
+++ public/main.js
@@ -0,0 +1,91 @@
+// const API_URL = `https://ourbox.herokuapp.com/api`;
+const API_URL = `http://localhost:5000/api`;
+
+async function showFileList() {
+    try {
+        let result = await fetch(API_URL);
+        result = await result.json();
+        for await (const file of result) {
+            const fileTable = document.getElementById('file-table');
+            const row = document.createElement('tr');
+            const nameCol = document.createElement('td');
+            const downloadAnchor = document.createElement('a');
+            downloadAnchor.download = file.name;
+            downloadAnchor.href = await getDownloadUrl(file.fullPath);
+            downloadAnchor.innerHTML = file.name;
+            nameCol.appendChild(downloadAnchor);
+            const sizeCol = document.createElement('td');
+            sizeCol.innerHTML = file.size;
+            const updatedCol = document.createElement('td');
+            updatedCol.innerHTML = file.updated;
+            const delButtonCol = document.createElement('td');
+            const deleteButton = document.createElement('button');
+            deleteButton.innerHTML = 'Delete';
+            deleteButton.onclick = () => { deleteItem(file.fullPath) };
+            delButtonCol.appendChild(deleteButton);
+            row.appendChild(nameCol);
+            row.appendChild(sizeCol);
+            row.appendChild(updatedCol);
+            row.appendChild(delButtonCol);
+            fileTable.appendChild(row);
+        }
+    } catch (err) {
+        console.log(err);
+        alert('File listing failed');
+    }
+}
+
+async function deleteItem(path) {
+    try {
+        const result = await fetch(API_URL, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                path
+            })
+        });
+        if (result.status === 200)
+            alert('Deleted Successfully. Refresh to update the UI.');
+        else
+            alert('Delete failed');
+    } catch (err) {
+        console.log(err);
+        alert('Delete failed');
+    }
+}
+
+async function getDownloadUrl(path) {
+    try {
+        let result = await fetch(`${API_URL}/download`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                path
+            })
+        });
+        result = await result.text();
+        return result;
+    } catch (err) {
+        console.log(err);
+        return '';
+    }
+}
+
+async function upload(event) {
+    try {
+        const uploader = document.getElementById('file-uploader');
+        let result = await fetch(API_URL, {
+            method: 'POST',
+            body: new FormData(uploader)
+        });
+        result = await result.json();
+        console.log(result);
+        alert('Refresh to view the updates');
+    } catch (err) {
+        console.log(err);
+    }
+}
